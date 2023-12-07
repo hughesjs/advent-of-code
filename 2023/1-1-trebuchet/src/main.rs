a493196ecf64a2644a3977de1747daab65534811
James Hughes <james@pyrosoftsolutions.co.uk>
2023-12-07 21:49:37 +0000

    Made things a bit more rustic

--- 2023/1-1-trebuchet/src/main.rs
+++ 2023/1-1-trebuchet/src/main.rs
@@ -1,47 +1,36 @@
+use std::error::Error;
 use std::io;
+use std::num::ParseIntError;
 use std::str::FromStr;
 
-fn main() {
-    let mut buff: String = String::new();
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut buf: String = String::new();
     let mut acc: u16 = 0;
-    while io::stdin().read_line(&mut buff).is_ok_and(|s| s > 0) {
-        let mut first_number: Option<char> = None;
-        let mut last_number: Option<char> = None;
-        // forward pass
-        for c in buff.chars() {
-            if c.is_numeric() {
-                first_number = Some(c);
-                break;
-            }
-        }
-        // backward pass
-        for c in buff.chars().rev() {
-            if c.is_numeric() {
-                last_number = Some(c);
-                break;
-            }
-        }
-
-        let first_char = match first_number {
-            None => '\0',
-            Some(c) => c
-        };
-
-        let last_char = match last_number {
-            None => '\0',
-            Some(c) => c
-        };
-
-        let char_string = format!("{first_char}{last_char}");
-        let val = u16::from_str(&*char_string);
-
-        acc += match val {
-            Ok(v) => v,
-            Err(_) => 0
-        };
-
-        buff = String::new();
+    while io::stdin().read_line(&mut buf).is_ok_and(|s| s > 0) {
+        acc += outside_digit_val(&*buf)?;
+        buf = String::new();
     }
 
     dbg!(acc);
+    Ok(())
+}
+
+fn outside_digit_val(buf: &str) -> Result<u16, ParseIntError> {
+    let mut new_buf: String = String::new();
+    // forward pass
+    for c in buf.chars() {
+        if c.is_numeric() {
+            new_buf.push(c);
+            break;
+        }
+    }
+    // backward pass
+    for c in buf.chars().rev() {
+        if c.is_numeric() {
+            new_buf.push(c);
+            break;
+        }
+    }
+
+    return u16::from_str(&*new_buf);
 }

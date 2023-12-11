307a27cd63afccaf150ab34a58e0a56a4d514541
James Hughes <james@pyrosoftsolutions.co.uk>
2023-12-11 20:58:01 +0000

    Refactored to be testable

--- 2023/1-1-trebuchet/src/main.rs
+++ 2023/1-1-trebuchet/src/main.rs
@@ -1,22 +1,38 @@
 use std::error::Error;
-use std::io;
+use std::fs;
+use std::env::Args;
 use std::num::ParseIntError;
 use std::str::FromStr;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut buf: String = String::new();
-    let mut acc: u16 = 0;
-    while io::stdin().read_line(&mut buf).is_ok_and(|s| s > 0) {
-        acc += outside_digit_val(&*buf)?;
-        buf = String::new();
-    }
-
-    dbg!(acc);
+    let file_path = parse_args(std::env::args())?;
+    let data = fs::read_to_string(file_path)?;
+    let res = calculate_puzzle(&data)?;
+    print!("Result: {}", res);
     Ok(())
 }
 
+fn parse_args(mut args: Args) -> Result<String, Box<dyn Error>>{
+    match args.nth(1)     {
+        Some(s) => Ok(s),
+        None => Err("No arguments provided".into())
+    }
+}
+
+fn calculate_puzzle(data: &str) -> Result<u16, Box<dyn Error>> {
+    let acc = data.split('\n').try_fold(0, |acc, token| {
+        let line_val = outside_digit_val(token)?;
+        Ok(acc + line_val)
+    });
+    return acc;
+}
+
 fn outside_digit_val(buf: &str) -> Result<u16, ParseIntError> {
-    let mut new_buf: String = String::new();
+    if buf.trim().is_empty() // Skip empty lines
+    {
+        return Ok(0);
+    }
+    let mut new_buf: String = String::with_capacity(2);
     // forward pass
     for c in buf.chars() {
         if c.is_numeric() {
@@ -33,4 +49,20 @@ fn outside_digit_val(buf: &str) -> Result<u16, ParseIntError> {
     }
 
     return u16::from_str(&*new_buf);
+}
+
+mod tests {
+    use super::*;
+    # [test]
+    fn run_test_data() {
+        let test_data = r#"
+                            1abc2
+                            pqr3stu8vwx
+                            a1b2c3d4e5f
+                            treb7uchet
+                            "#;
+
+        let res = calculate_puzzle(test_data).unwrap();
+        assert_eq!(142, res);
+    }
 }

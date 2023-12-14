5f673b1cff94efe56a14036fbf5d34042e53d703
James Hughes <james.hughes@spektrix.com>
2023-12-14 15:47:23 +0000

    Converted test-data to raw-string-literal (technically not standard c but meh...)

--- 2023/02-cubes/part-1/tests/tests.c
+++ 2023/02-cubes/part-1/tests/tests.c
@@ -2,7 +2,11 @@
 #include "logicker.h"
 
 void provided_test_case(void) {
-    const char* testData = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    const char* testData = R"(Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green)";
 
     const int expected = 8;
 

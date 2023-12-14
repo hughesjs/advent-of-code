dd3647f0ec5d61de02126330af6ab0b32fbf06e7
James Hughes <james@pyrosoftsolutions.co.uk>
2023-12-14 18:18:48 +0000

    Added tests for string utils

--- 2023/02-cubes/part-1/tests/string_utils_tests.c
+++ 2023/02-cubes/part-1/tests/string_utils_tests.c
@@ -1,12 +1,28 @@
-#include <unity_internals.h>
+#include <unity.h>
 
 #include "string_utils.h"
 
 void can_split_string(void) {
+    const char* test_data = "123;456;789";
+    int resCount;
+    const char** results = splitString(test_data, ";", &resCount);
 
+    TEST_ASSERT_EQUAL_INT(3, resCount);
+    TEST_ASSERT_EQUAL_STRING("123", results[0]);
+    TEST_ASSERT_EQUAL_STRING("456", results[1]);
+    TEST_ASSERT_EQUAL_STRING("789", results[2]);
+}
+
+void source_string_unaffected_after_split(void) {
+    const char* test_data = "123;456;789";
+    int resCount;
+    splitString(test_data, ";", &resCount);
+
+    TEST_ASSERT_EQUAL_STRING("123;456;789", test_data);
 }
 
 void run_string_tests(void) {
     RUN_TEST(can_split_string);
+    RUN_TEST(source_string_unaffected_after_split);
 }
 

5c2de37858229efaed6b91e45b513ba5ae835548
James Hughes <james@pyrosoftsolutions.co.uk>
2023-12-14 22:09:01 +0000

    Fixed not closing file

--- 2023/02-cubes/part-1/src/main.c
+++ 2023/02-cubes/part-1/src/main.c
@@ -4,8 +4,7 @@
 
 #include "logicker.h"
 
-int main(const int argc, const char** argv)
-{
+int main(const int argc, const char** argv) {
     if (!argc == 1) {
         printf("Ensure you're passing a file-path to your input data");
         return -1;
@@ -14,12 +13,13 @@ int main(const int argc, const char** argv)
     const char* dataPath = argv[1];
 
     FILE* file = fopen(dataPath, "r");
+
     struct stat fileStatBuf;
     fstat(fileno(file), &fileStatBuf);
 
     char* fileContents = malloc(fileStatBuf.st_size + 1);
     fread(fileContents, fileStatBuf.st_size, fileStatBuf.st_size, file);
-
+    fclose(file);
     const int answer = deduceAnswer(fileContents, 12, 13, 14);
     printf("Answer: %d", answer);
 

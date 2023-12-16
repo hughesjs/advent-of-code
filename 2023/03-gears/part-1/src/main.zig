4b40be9af0dd899ac22d45a54a3e8b4c16c166cc
James Hughes <james@pyrosoftsolutions.co.uk>
2023-12-16 18:01:05 +0000

    Can read the schematic from a filepath arg passed in

--- 2023/03-gears/part-1/src/main.zig
+++ 2023/03-gears/part-1/src/main.zig
@@ -1,20 +1,44 @@
 const std = @import("std");
+const allocator = std.heap.page_allocator;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const fPath = try get_file_path_from_args();
+    defer allocator.free(fPath);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    const engine_schematic = try get_schematic_from_file_path(fPath);
+    defer allocator.free(engine_schematic);
 }
+
+fn get_schematic_from_file_path(fPath: []const u8) ![]const u8 {
+    const input_data_file = try std.fs.cwd().openFile(fPath, .{ });
+    defer input_data_file.close();
+
+    const file_size = try input_data_file.getEndPos();
+    std.log.debug("File Size: {d}", .{file_size});
+
+    const buffer: []u8 = try input_data_file.readToEndAlloc(allocator, file_size);
+    std.log.debug("Schematic:\n{s}", .{buffer});
+    return buffer;
+}
+
+fn get_file_path_from_args() ![]const u8 {
+        const args = try std.process.argsAlloc(allocator);
+        defer std.process.argsFree(allocator, args);
+
+        std.debug.print("Number of arguments: {d}\n", .{args.len});
+        for (args) |arg| {
+            std.debug.print("Argument: {s}\n", .{arg});
+        }
+
+        const pathBuf = try std.mem.Allocator.dupe(allocator, u8, args[1]);
+        std.debug.print("fpath: {s}\n", .{pathBuf});
+        return pathBuf;
+}
+
+fn get_part_codes(engine_schematic: *[]u8) std.ArrayList([]u8) {
+    _ = engine_schematic;
+}
+
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
@@ -22,4 +46,23 @@ test "simple test" {
     try list.append(42);
 
     try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
+
+test "provided_test_case" {
+    const testData =
+        \\467..114..
+        \\...*......
+        \\..35..633.
+        \\......#...
+        \\617*......
+        \\.....+.58.
+        \\..592.....
+        \\......755.
+        \\...$.*....
+        \\.664.598..
+    ;
+
+    const res = get_part_codes(testData);
+
+    try std.testing.expectEqual(testData, res);
 }

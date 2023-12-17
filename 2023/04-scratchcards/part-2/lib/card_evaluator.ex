708d2033ae99dc40be905333643db2df99e49879
James H <james@pyrosoftsolutions.co.uk>
2023-12-17 20:57:31 +0000

    Scratchcards Part 2 (#17)
    * Set up part 2
    
    Signed-off-by: James Hughes <james@pyrosoftsolutions.co.uk>
    
    * Wip recursive change
    
    * Made it work for part2 and added final value check. Also removed IO for SPEED
    
    ---------
    
    Signed-off-by: James Hughes <james@pyrosoftsolutions.co.uk>

--- 2023/04-scratchcards/part-2/lib/card_evaluator.ex
+++ 2023/04-scratchcards/part-2/lib/card_evaluator.ex
@@ -4,12 +4,7 @@ defmodule CardEvaluator do
     |> Enum.filter(&Enum.member?(card.result_numbers, &1))
     |> Enum.count()
 
-    value_of_card = if (num_matches > 0) do
-      :math.pow(2, num_matches - 1)
-    else
-      0
-    end
-
-    value_of_card
+    #IO.puts("Value of card ##{card.id} is #{num_matches}")
+    num_matches
   end
 end
--- 2023/04-scratchcards/part-2/lib/scratchcards.ex
+++ 2023/04-scratchcards/part-2/lib/scratchcards.ex
@@ -1,17 +1,38 @@
 defmodule Scratchcards do
-  def process_scratchcards(card_data) do
-    total_val = card_data
-    |> Enum.map(&CardParser.parse_card/1)
-    |> Enum.map(&CardEvaluator.evaluate_card/1)
-    |> Enum.sum()
+  def process_scratchcards(card_strings) do
+    cards =
+      card_strings
+      |> Enum.map(&CardParser.parse_card/1)
 
-    IO.puts("Answer: #{total_val}")
-    total_val
+    total_reward = cards
+      |> Enum.map(fn card -> process_card_reward(card, cards) end)
+      |> Enum.sum()
+
+    total_reward
   end
 
+  def process_card_reward(card, all_cards) do
+    value =
+      card
+      |> CardEvaluator.evaluate_card()
+
+    total = if (value > 0) do
+      victory_range = (card.id + 1)..(card.id + value)
+
+      num_children = Enum.reduce(victory_range, 0, fn n, acc ->
+          acc + process_card_reward(Enum.at(all_cards,n-1), all_cards)
+        end)
+      num_children + 1
+    else
+      1
+    end
+
+    total
+  end
 
   def process_scratchcards_from_file(input_file) do
-    FileReader.read_scratchcard_file(input_file)
+    res = FileReader.read_scratchcard_file(input_file)
     |> process_scratchcards()
+    IO.puts("Answer: #{res}")
   end
 end

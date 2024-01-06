0f6b5c978eb24f29a848350eda35e57a609045bb
James Hughes <james@pyrosoftsolutions.co.uk>
2024-01-06 03:45:40 +0000

    Read file as string from arg
    Signed-off-by: James Hughes <james@pyrosoftsolutions.co.uk>

--- 2023/05-seeds/part-1/main.go
+++ 2023/05-seeds/part-1/main.go
@@ -1,7 +1,12 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	fmt.Println("Hello, world.")
+	args := os.Args
+	fPath := args[1]
+	data, _ := os.ReadFile(fPath)
+	content := string(data)
+	fmt.Println(content)
 }

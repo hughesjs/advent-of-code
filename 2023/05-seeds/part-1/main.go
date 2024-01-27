4f1c9ce47c1c0cc7227762dc7eb661fd4e5cd339
James Hughes <james@pyrosoftsolutions.co.uk>
2024-01-27 22:50:28 +0000

    Fixed naming to match go style

--- 2023/05-seeds/part-1/main.go
+++ 2023/05-seeds/part-1/main.go
@@ -8,46 +8,46 @@ import (
 )
 
 func main() {
-	input_data := get_input_data()
-	answer := parse_data(input_data)
+	inputData := getInputData()
+	answer := parseData(inputData)
 	fmt.Println("Answer: {}", answer)
 }
 
-func parse_data(input_data string) int32 {
-	segments := strings.Split(input_data, "\n\n")
-	seeds := parse_seeds(segments[0])
+func parseData(inputData string) int32 {
+	segments := strings.Split(inputData, "\n\n")
+	seeds := parseSeeds(segments[0])
 	fmt.Println(seeds)
-	maps := parse_maps(segments[1:])
+	maps := parseMaps(segments[1:])
 	fmt.Println(maps)
 	return 0
 }
 
-func parse_maps(segments []string) map[int][]seed_map_entry {
-	maps := make(map[int][]seed_map_entry)
+func parseMaps(segments []string) map[int][]seedMapEntry {
+	maps := make(map[int][]seedMapEntry)
 	for i, seg := range segments {
-		maps[i] = parse_map_seg(seg)
+		maps[i] = parseMapSeg(seg)
 	}
 	return maps
 }
 
-func parse_map_seg(seg string) []seed_map_entry {
-	var map_entries []seed_map_entry
-	entry_lines := strings.Split(seg, "\n")
+func parseMapSeg(seg string) []seedMapEntry {
+	var mapEntries []seedMapEntry
+	entryLines := strings.Split(seg, "\n")
 
-	for _, line := range entry_lines[1:] {
+	for _, line := range entryLines[1:] {
 		if line == "" {
 			continue
 		}
-		part_strings := strings.Split(line, " ")
-		parts := string_arr_to_int_arr(part_strings)
-		entry := seed_map_entry{dest_range_start: parts[0], source_range_start: parts[1], range_length: parts[2]}
-		map_entries = append(map_entries, entry)
+		partStrings := strings.Split(line, " ")
+		parts := stringArrToIntArr(partStrings)
+		entry := seedMapEntry{destRangeStart: parts[0], sourceRangeStart: parts[1], rangeLength: parts[2]}
+		mapEntries = append(mapEntries, entry)
 	}
 
-	return map_entries
+	return mapEntries
 }
 
-func string_arr_to_int_arr(arr []string) []int32 {
+func stringArrToIntArr(arr []string) []int32 {
 	var ints []int32
 
 	for _, val := range arr {
@@ -58,13 +58,13 @@ func string_arr_to_int_arr(arr []string) []int32 {
 	return ints
 }
 
-func parse_seeds(seed_def string) []int32 {
-	data := strings.Replace(seed_def, "seeds: ", "", -1)
+func parseSeeds(seedDef string) []int32 {
+	data := strings.Replace(seedDef, "seeds: ", "", -1)
 	seeds := strings.Split(data, " ")
-	return string_arr_to_int_arr(seeds)
+	return stringArrToIntArr(seeds)
 }
 
-func get_input_data() string {
+func getInputData() string {
 	args := os.Args
 	fPath := args[1]
 	data, _ := os.ReadFile(fPath)
@@ -72,10 +72,10 @@ func get_input_data() string {
 	return content
 }
 
-type seed_map_entry struct {
-	dest_range_start   int32
-	source_range_start int32
-	range_length       int32
+type seedMapEntry struct {
+	destRangeStart   int32
+	sourceRangeStart int32
+	rangeLength      int32
 }
 
 const (
